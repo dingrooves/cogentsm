21cd2a0b2e5c0b43dd4d669c8a8041aa7b6d730e
dingrooves <dinesh@plinetech.com>
2017-09-28 14:18:42 +0530

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -48,3 +48,20 @@ function createContentPage() {
 }
 
 //addition by DG
+$(document).ready(function () {
+    document.addEventListener("deviceready", onDeviceReady, false);
+ });
+
+function onDeviceReady() {
+    window.plugins.phonenumber.get(success, failed);
+
+    document.addEventListener("backbutton", onBackKeyDown, false);
+}
+
+function success(phonenumber) {
+    document.getElementById("myphone").value=phonenumber;
+}
+
+function failed(phonenumber) {
+    document.getElementById("myphone").value="00";
+}
